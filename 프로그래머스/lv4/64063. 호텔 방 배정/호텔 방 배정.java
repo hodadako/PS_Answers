47be8e71e5963aed86c19ecfb5e123c6834c9098
isevou__ <kbry12@naver.com>
2023-08-19 15:49:21 +0900

    [level 4] Title: 호텔 방 배정, Time: 248.25 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -1,72 +1,65 @@
 import java.util.*;
 
 class Solution {
-    private static class Node {
+    class Node {
         private int depth = 1;
         private Node parent = null;
-
+        
         private long max;
-
-        public Node (long cur) {
-            max = cur;
+        
+        public Node (long max) {
+            this.max = max;
         }
-
-        public boolean isConnected(Node o) {
-            return root() == o.root();
-        }
-
+        
         public long max() {
             return root().max;
         }
-
+        
+        public boolean isConnected(Node o) {
+            return root() == o.root();
+        }
+        
         public void merge(Node o) {
             if (isConnected(o)) return;
+            
             Node root1 = root();
             Node root2 = o.root();
-
             if (root1.depth > root2.depth) {
                 root2.parent = root1;
             } else if (root1.depth < root2.depth) {
                 root1.parent = root2;
             } else {
                 root2.parent = root1;
-                root1.depth += 1;
+                root1.depth++;
             }
-
+            
             root1.max = root2.max = Math.max(root1.max, root2.max);
         }
-
-        private Node root() {
+        
+        public Node root() {
             if (parent == null) return this;
             return parent.root();
         }
     }
-
-    public static long[] solution(long k, long[] room_number) {
-        HashMap<Long, Node> key = new HashMap<>();
+    
+    public long[] solution(long k, long[] room_number) {
         ArrayList<Long> answer = new ArrayList<>();
-        for (long now : room_number) {
-            if (key.containsKey(now)) {
-                now = key.get(now).max() + 1;
+        HashMap<Long, Node> count = new HashMap<>();
+        for (long number : room_number) {
+            if (count.containsKey(number)) {
+                number = count.get(number).max() + 1;
             }
-
-            Node node = new Node(now);
-            key.put(now, node);
-            if (key.containsKey(now - 1)) {
-                node.merge(key.get(now - 1));
+            
+            Node node = new Node(number);
+            answer.add(number);
+            count.put(number, node);
+            if (count.containsKey(number - 1)) {
+                node.merge(count.get(number - 1));
             }
-            if (key.containsKey(now + 1)) {
-                node.merge(key.get(now + 1));
+            if (count.containsKey(number + 1)) {
+                node.merge(count.get(number + 1));
             }
-
-            answer.add(now);
         }
-
         return answer.stream().mapToLong(Long::longValue).toArray();
-    }
-
-    public static void main(String[] args) {
-        long k = 10L;
-        long[] room_number = {1, 3, 4, 1, 3, 1};
     }
 }
