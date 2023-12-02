ff1dcd7bf54b9ca6448b08748a34857018a9f2cb
Byungryong Ko <kbry12@naver.com>
2023-12-03 00:25:27 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 336 ms, Memory: 18564 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+    
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+    
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+    
+            return st.nextToken();
+        }
+    
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        int n = fr.nextInt();
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (n - i) * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
