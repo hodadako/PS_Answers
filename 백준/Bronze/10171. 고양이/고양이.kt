522c447a736ab3d46efa336fc02bb79b6e74a11a
Byungryong Ko <kbry12@naver.com>
2024-07-14 23:17:37 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13396 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.kt
@@ -0,0 +1,8 @@
+import java.util.*;
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    println("\\    /\\\n" +
+            " )  ( ')\n" +
+            "(  /  )\n" +
+            " \\(__)|")
+}
