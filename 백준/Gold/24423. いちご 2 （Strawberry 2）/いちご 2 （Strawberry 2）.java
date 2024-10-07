8e61386dc17540d5ada4380df1b22c26ab18dff4
Byungryong Ko <kbry12@naver.com>
2024-10-07 21:11:15 +0900

    [Gold V] Title: いちご 2 (Strawberry 2), Time: 872 ms, Memory: 135828 KB, Score: 74 point -BaekjoonHub

--- 백준/Gold/24423. いちご 2 （Strawberry 2）/いちご 2 （Strawberry 2）.java
+++ 백준/Gold/24423. いちご 2 （Strawberry 2）/いちご 2 （Strawberry 2）.java
@@ -1,39 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-	public static class FastReader {
-	    BufferedReader br;
-	    StringTokenizer st;
-
-	    public FastReader() {
-	        br = new BufferedReader(new InputStreamReader(System.in));
-	    }
-
-	    String next() {
-	        while (st == null || !st.hasMoreElements()) {
-	            try {
-	                st = new StringTokenizer(br.readLine());
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	        return st.nextToken();
-	    }
-
-	    int nextInt() {
-	        return Integer.parseInt(next());
-	    }
-
-	    double nextDouble() {
-	        return Double.parseDouble(next());
-	    }
-
-	    long nextLong() {
-	        return Long.parseLong(next());
-	    }
-	}
-
+public class Main {
 	static class Node {
 		int x, y;
 
@@ -44,12 +12,11 @@ class Main {
 
 		@Override
 		public boolean equals(Object o) {
-			if (this == o)
-				return true;
-			if (o == null || getClass() != o.getClass())
-				return false;
-			Node node = (Node)o;
-			return x == node.x && y == node.y;
+			if (o instanceof Node) {
+				Node other = (Node) o;
+				return this.x == other.x && this.y == other.y;
+			}
+			return false;
 		}
 
 		@Override
@@ -57,13 +24,33 @@ class Main {
 			return (x + "," + y).hashCode();
 		}
 	}
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 
 	static int H, W, N;
-	static HashSet<Node> nodes = new HashSet<>();
-	static int[][] graph;
+	static HashMap<Node, Integer> countMap = new HashMap<>();
 	static int answer = 0;
-	static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-	static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
 
 	public static void main(String[] args) {
 		FastReader fr = new FastReader();
@@ -71,52 +58,23 @@ class Main {
 		W = fr.nextInt();
 		N = fr.nextInt();
 
-		graph = new int[H][W];
-
 		for (int i = 0; i < N; i++) {
 			int a = fr.nextInt() - 1;
 			int b = fr.nextInt() - 1;
 
-			graph[a][b]++;
-			nodes.add(new Node(a, b));
-		}
-
-		for (Node node : nodes) {
-			count(node);
+			for (int x = -1; x <= 1; x++) {
+				for (int y = -1; y <= 1; y++) {
+					int nx = a + x;
+					int ny = b + y;
+					if (nx >= 1 && nx < H - 1 && ny >= 1 && ny < W - 1) {
+						Node key = new Node(nx, ny);
+						countMap.put(key, countMap.getOrDefault(key, 0) + 1);
+						answer = Math.max(answer, countMap.get(key));
+					}
+				}
+			}
 		}
 
 		System.out.println(answer);
-	}
-
-	private static void count(Node node) {
-		int result = 0;
-		ArrayList<Node> nodeArrayList = new ArrayList<>();
-		for (int i = 0; i < 8; i++) {
-			int nx = node.x + dx[i];
-			int ny = node.y + dy[i];
-
-			if (nx < 0 || nx >= H || ny < 0 || ny >= W) {
-				continue;
-			}
-			result += graph[nx][ny];
-			nodeArrayList.add(new Node(nx, ny));
-		}
-		result += graph[node.x][node.y];
-		answer = Math.max(answer, result);
-
-		for (Node now : nodeArrayList) {
-			int nowResult = 0;
-			for (int i = 0; i < 8; i++) {
-				int nx = now.x + dx[i];
-				int ny = now.y + dy[i];
-
-				if (nx < 0 || nx >= H || ny < 0 || ny >= W) {
-					continue;
-				}
-				nowResult += graph[nx][ny];
-			}
-			nowResult += graph[now.x][now.y];
-			answer = Math.max(answer, nowResult);
-		}
 	}
 }
