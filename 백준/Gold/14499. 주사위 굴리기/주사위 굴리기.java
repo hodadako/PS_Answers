d29ca8cb3993f114922c810e95d5495faf4cf8dc
Byungryong Ko <kbry12@naver.com>
2025-02-11 23:19:45 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 116 ms, Memory: 12748 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -1,148 +1,173 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-    static int n, m;
-    static int[][] map;
-    static int[] orders;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
 
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 
-            return st.nextToken();
-        }
+	static void roll(int order, Dice dice) {
+		int nx = x + dx[order];
+		int ny = y + dy[order];
+		if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+			return;
+		}
+		x = nx;
+		y = ny;
 
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-    }
+		if (order == 1) {
+			rollEast(dice);
+		} else if (order == 2) {
+			rollWest(dice);
+		} else if (order == 3) {
+			rollNorth(dice);
+		} else {
+			rollSouth(dice);
+		}
 
-    static class Dice {
-        int top = 1, now = 6, x, y;
-        int[] status = new int[7];
-        int[][] next = {{0, 2, 0}, {4, 6, 3}, {0, 5, 0}, {0, 1, 0}};
+		dice.printTop();
+	}
 
-        void print() {
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(next[i][j] + " ");
-                }
-                System.out.println();
-            }
-        }
+	static void rollEast(Dice dice) {
+		int temp = dice.right;
+		dice.right = dice.top;
+		dice.top = dice.left;
+		dice.left = dice.bottom;
+		if (map[x][y] == 0) {
+			dice.bottom = temp;
+			map[x][y] = dice.bottom;
+		} else {
+			dice.bottom = map[x][y];
+			map[x][y] = 0;
+		}
+	}
 
-        void move(int order) {
-            if (order == 1) {
-                right();
-            } else if (order == 2) {
-                left();
-            } else if (order == 3) {
-                up();
-            } else {
-                down();
-            }
-        }
+	static void rollWest(Dice dice) {
+		int temp = dice.left;
+		dice.left = dice.top;
+		dice.top = dice.right;
+		dice.right = dice.bottom;
+		if (map[x][y] == 0) {
+			dice.bottom = temp;
+			map[x][y] = dice.bottom;
+		} else {
+			dice.bottom = map[x][y];
+			map[x][y] = 0;
+		}
+	}
 
-        void down() {
-            int temp = next[0][1];
-            for (int i = 0; i < 3; i++) {
-               next[i][1] = next[i + 1][1];
-            }
-            next[3][1] = temp;
-            top = next[3][1];
-            now = next[1][1];
-        }
+	static void rollNorth(Dice dice) {
+		int temp = dice.rear;
+		dice.rear = dice.top;
+		dice.top = dice.front;
+		dice.front = dice.bottom;
+		if (map[x][y] == 0) {
+			dice.bottom = temp;
+			map[x][y] = dice.bottom;
+		} else {
+			dice.bottom = map[x][y];
+			map[x][y] = 0;
+		}
+	}
 
-        void up() {
-            int temp = next[3][1];
-            for (int i = 3; i > 0; i--) {
-                next[i][1] = next[i - 1][1];
-            }
-            next[0][1] = temp;
-            top = next[3][1];
-            now = next[1][1];
-        }
-        void left() {
-            int temp = next[1][2];
-            for (int i = 2; i > 0; i--) {
-                next[1][i] = next[1][i - 1];
-            }
-            next[1][0] = next[3][1];
-            next[3][1] = temp;
-            top = next[3][1];
-            now = next[1][1];
-        }
-
-        void right() {
-            int temp = next[1][0];
-            for (int i = 0; i < 2; i++) {
-                next[1][i] = next[1][i + 1];
-            }
-            next[1][2] = next[3][1];
-            next[3][1] = temp;
-            top = next[3][1];
-            now = next[1][1];
-        }
-
-        public Dice(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    public static void main(String[] args) {
-        FastReader sc = new FastReader();
-        n = sc.nextInt();
-        m = sc.nextInt();
-        int x = sc.nextInt(), y = sc.nextInt(), k = sc.nextInt();
-
-        map = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                map[i][j] = sc.nextInt();
-            }
-        }
-
-        Dice dice = new Dice(x, y);
-        orders = new int[k];
-        for (int i = 0; i < k; i++) {
-            orders[i] = sc.nextInt();
-            int nx = dice.x + dx[orders[i] - 1], ny = dice.y + dy[orders[i] - 1];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            dice.x = nx;
-            dice.y = ny;
-            dice.move(orders[i]);
-//            System.out.println("dice.now = " + dice.now);
-//            System.out.println("orders[i] = " + orders[i]);
-//            System.out.println("dots = " + dice.x + " " + dice.y);
-//            dice.print();
-//            System.out.println("dice.status = " + Arrays.toString(dice.status));
-            if (map[dice.x][dice.y] == 0) {
-                map[dice.x][dice.y] = dice.status[dice.now];
-            } else {
-                dice.status[dice.now] = map[dice.x][dice.y];
-                map[dice.x][dice.y] = 0;
-            }
+	static void rollSouth(Dice dice) {
+		int temp = dice.front;
+		dice.front = dice.top;
+		dice.top = dice.rear;
+		dice.rear = dice.bottom;
+		if (map[x][y] == 0) {
+			dice.bottom = temp;
+			map[x][y] = dice.bottom;
+		} else {
+			dice.bottom = map[x][y];
+			map[x][y] = 0;
+		}
+	}
 
 
-            System.out.println(dice.status[dice.top]);
-//            System.out.println("_____________________________________");
-        }
-    }
+	static class Dice {
+		int top, bottom, left, right, front, rear;
+
+		public Dice() {
+			this.top = 0;
+			this.bottom = 0;
+			this.left = 0;
+			this.right = 0;
+			this.front = 0;
+			this.rear = 0;
+		}
+
+		void printTop() {
+			System.out.println(this.top);
+		}
+
+		@Override
+		public String toString() {
+			System.out.println("________________________");
+			return "  " + front + " \n" + left + " " + top + " " + right + " \n  " + rear + " \n  " + bottom;
+		}
+	}
+
+	static int N, M, x, y, K;
+	static int[][] map;
+	static int[] dx = {0, 0, 0, -1, 1};
+	static int[] dy = {0, 1, -1, 0, 0};
+
+	static void printMap() {
+		System.out.println("_____________________________");
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("_____________________________");
+	}
+
+	public static void main(String[] args) {
+		FastReader fr = new FastReader();
+		N = fr.nextInt();
+		M = fr.nextInt();
+		x = fr.nextInt();
+		y = fr.nextInt();
+		K = fr.nextInt();
+
+		map = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				map[i][j] = fr.nextInt();
+			}
+		}
+
+		Dice dice = new Dice();
+
+		for (int i = 0; i < K; i++) {
+			int now = fr.nextInt();
+			roll(now, dice);
+			// System.out.println("order = " + now);
+			// System.out.println(dice);
+			// printMap();
+		}
+
+	}
 }
