487f5986d9ebd4ebc47c21cfeca45d5f1b410592
Byungryong Ko <kbry12@naver.com>
2025-02-13 19:28:50 +0900

    [Gold II] Title: 새로운 게임 2, Time: 128 ms, Memory: 17220 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.java
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.java
@@ -1,222 +1,213 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    static boolean con = true;
-    static int n, k;
-    static int[][] map;
-    static Node[] nodes;
-    static ArrayList<Integer>[][] count;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-    public static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
 
-            return st.nextToken();
-        }
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
+	static class Node {
+		int x, y, dir, num;
 
-        long nextLong() {
-            return Long.parseLong(next());
-        }
+		public Node(int x, int y, int dir, int num) {
+			this.x = x;
+			this.y = y;
+			this.dir = dir;
+			this.num = num;
+		}
 
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-    }
+		@Override
+		public boolean equals(Object o) {
+			if (o instanceof Node) {
+				Node n = (Node) o;
+				return n.num == this.num;
+			}
+			return false;
+		}
 
-    static class Node {
-        int x, y, dir, num;
+		@Override
+		public int hashCode() {
+			return (num + ", ").hashCode();
+		}
 
-        public Node(int x, int y, int dir, int num) {
-            this.x = x;
-            this.y = y;
-            this.dir = dir;
-            this.num = num;
-        }
+		@Override
+		public String toString() {
+			return "(" + x + ", " + y + ", " + dir + ", " + num + ")";
+		}
+	}
 
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof Node)) return false ;
-            Node node = (Node) o;
-            return num == node.num && dir == node.dir;
-        }
+	static int N, K, count;
+	static boolean end = false;
+	static int[][] board;
+	static int[] dx = {0, 0, -1, 1};
+	static int[] dy = {1, -1, 0, 0};
+	static HashMap<Integer, Node> map = new HashMap<>();
+	static ArrayList<Node>[][] nodes;
 
-        @Override
-        public int hashCode() {
-            return (num + "," + dir).hashCode();
-        }
-    }
+	static int blue(int currentDir) {
+		if (currentDir == 0) {
+			return 1;
+		} else if (currentDir == 1) {
+			return 0;
+		} else if (currentDir == 2) {
+			return 3;
+		}
+		return 2;
+	}
 
-    private static int uTurn(int dir) {
-        int result = 0;
-        if (dir == 0) result = 1;
-        else if (dir == 1) result = 0;
-        else if (dir == 2) result = 3;
-        else if (dir == 3) result = 2;
-        return result;
-    }
+	static boolean checkWhite(int x, int y) {
+		return board[x][y] == 0;
+	}
 
-    private static int[] getNextMove(int num) {
-        int[] nextMove = new int[2];
-        boolean twice = false;
-        Node node = nodes[num];
-        int nx = node.x + dx[node.dir], ny = node.y + dy[node.dir];
-        int nDir = uTurn(node.dir);
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
-            nx = node.x + dx[nDir];
-            ny = node.y + dy[nDir];
-            nodes[num].dir = nDir;
-            twice = true;
-        }
+	static boolean checkRed(int x, int y) {
+		return board[x][y] == 1;
+	}
 
-        if (map[nx][ny] == 0) {
-            nextMove[0] = node.dir;
-//            System.out.println("white");
-        } else if (map[nx][ny] == 1) {
-            nextMove[0] = node.dir;
-            nextMove[1] = 1;
-//            System.out.println("red");
-        } else if (map[nx][ny] == 2) {
-//            System.out.println("blue");
-            if (twice) {
-                return null;
-            } else {
-                nodes[num].dir = nDir;
-                nx = node.x + dx[nDir];
-                ny = node.y + dy[nDir];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) return null;
-                if (map[nx][ny] == 0) {
-                    nextMove[0] = nodes[num].dir;
-                } else if (map[nx][ny] == 1) {
-                    nextMove[0] = nodes[num].dir;
-                    nextMove[1] = 1;
-                } else {
-                    return null;
-                }
-            }
-        }
-        return nextMove;
-    }
+	static boolean checkBlue(int x, int y) {
+		return board[x][y] == 2;
+	}
 
-    private static void move(int num) {
-//        System.out.println(num);
-        int[] nextMove = getNextMove(num);
-//        System.out.println(Arrays.toString(nextMove));
-        if (nextMove == null) {
-            return;
-        }
-        int x = nodes[num].x, y = nodes[num].y;
-        int nx = nodes[num].x + dx[nextMove[0]], ny = nodes[num].y + dy[nextMove[0]];
-        int index = count[x][y].indexOf(num);
-        ArrayList<Integer> temp = new ArrayList<>();
-        for (int i = index; i < count[x][y].size(); i++) {
-            int now = count[x][y].get(i);
-            temp.add(now);
-        }
+	static void printBoard(ArrayList<Node>[][] board) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("__________________________");
+	}
 
-        if (nextMove[1] == 1) {
-            Collections.reverse(temp);
-        }
+	static void move(Node now) {
+		int index = nodes[now.x][now.y].indexOf(now);
+		int nx = now.x + dx[now.dir];
+		int ny = now.y + dy[now.dir];
+		boolean checked = false;
+		ArrayList<Node> temp = new ArrayList<>();
 
-        ArrayList<Integer> original = new ArrayList<>();
-        for (int i : count[x][y]) {
-            if (!temp.contains(i)) {
-                original.add(i);
-            }
-        }
-        count[x][y] = original;
-        count[nx][ny].addAll(temp);
-        for (int i : temp) {
-            nodes[i].x = nx;
-            nodes[i].y = ny;
-        }
+		if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+			int newDir = blue(now.dir);
+			nx = now.x + dx[newDir];
+			ny = now.y + dy[newDir];
+			now.dir = newDir;
+			checked = true;
+		}
 
-        if (count[nx][ny].size() >= 4) {
-            con = false;
-        }
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                System.out.print(count[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("____________________");
-    }
+		if (checkBlue(nx, ny)) {
+			if (!checked) {
+				int newDir = blue(now.dir);
+				nx = now.x + dx[newDir];
+				ny = now.y + dy[newDir];
+				now.dir = newDir;
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+					return;
+				}
+				if (checkWhite(nx, ny)) {
+					temp.add(new Node(nx, ny, now.dir, now.num));
+					for (int j = index + 1; j < nodes[now.x][now.y].size(); j++) {
+						Node node = nodes[now.x][now.y].get(j);
+						temp.add(new Node(nx, ny, node.dir, node.num));
+					}
+				} else if (checkRed(nx, ny)) {
+					temp.add(new Node(nx, ny, now.dir, now.num));
+					for (int j = index + 1; j < nodes[now.x][now.y].size(); j++) {
+						Node node = nodes[now.x][now.y].get(j);
+						temp.add(new Node(nx, ny, node.dir, node.num));
+					}
+				} 
+			}
+		} else if (checkWhite(nx, ny)) {
+			temp.add(new Node(nx, ny, now.dir, now.num));
+			for (int j = index + 1; j < nodes[now.x][now.y].size(); j++) {
+				Node node = nodes[now.x][now.y].get(j);
+				temp.add(new Node(nx, ny, node.dir, node.num));
+			}
+		} else if (checkRed(nx, ny)) {
+			temp.add(new Node(nx, ny, now.dir, now.num));
+			for (int j = index + 1; j < nodes[now.x][now.y].size(); j++) {
+				Node node = nodes[now.x][now.y].get(j);
+				temp.add(new Node(nx, ny, node.dir, node.num));
+			}
+		}
 
-    private static void cycle() {
-        for (int i = 0; i < k; i++) {
-            move(i);
-            if (!con) break;
-        }
-    }
+		// System.out.println("temp = " + temp);
+		// System.out.println("nodes[now.x][now.y] = " + nodes[now.x][now.y]);
+		// System.out.println("index = " + index);
+		ArrayList<Node> original = new ArrayList<>();
+		for (int j = 0; j < nodes[now.x][now.y].size(); j++) {
+			Node node = nodes[now.x][now.y].get(j);
+			if (!temp.contains(node)) {
+				original.add(node);
+			}
+		}
 
-    private static boolean check() {
-        boolean result = false;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (count[i][j].size() >= 4) {
-                    result = true;
-                    break;
-                }
-            }
-        }
-        return result;
-    }
+		if (checkRed(nx, ny)) {
+			Collections.reverse(temp);
+		}
 
-    public static void main(String[] args) {
-        FastReader sc = new FastReader();
-        n = sc.nextInt();
-        k = sc.nextInt();
-        map = new int[n][n];
-        count = new ArrayList[n][n];
-        nodes = new Node[k];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                map[i][j] = sc.nextInt();
-                count[i][j] = new ArrayList<>();
-            }
-        }
 
-        for (int i = 0; i < k; i++) {
-            int x = sc.nextInt(), y = sc.nextInt(), dir = sc.nextInt();
-            Node node = new Node(x - 1, y - 1, dir - 1, i);
-            nodes[i] = node;
-            count[x - 1][y - 1].add(i);
-        }
+		for (Node n : temp) {
+			nodes[nx][ny].add(n);
+			map.put(n.num, n);
+		}
 
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                System.out.print(count[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("____________________");
+		if (nodes[nx][ny].size() >= 4) {
+			end = true;
+		}
+		nodes[now.x][now.y] = original;
+	}
 
-        int count = 0;
-        for (int i = 0; i < 1001; i++) {
-            count++;
-            cycle();
-            if (!con) break;
-        }
+	public static void main(String[] args) {
+		FastReader fr = new FastReader();
+		N = fr.nextInt();
+		K = fr.nextInt();
+		board = new int[N][N];
+		nodes = new ArrayList[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				board[i][j] = fr.nextInt();
+				nodes[i][j] = new ArrayList<>();
+			}
+		}
 
-        System.out.println(count > 1000 ? -1 : count);
-    }
+		for (int i = 0; i < K; i++) {
+			int x = fr.nextInt() - 1;
+			int y = fr.nextInt() - 1;
+			Node now = new Node(x, y, fr.nextInt() - 1, i);
+			map.put(i, now);
+			nodes[x][y].add(now);
+		}
+
+		while (count++ < 1001) {
+			for (int i = 0; i < K; i++) {
+				move(map.get(i));
+				// printBoard(nodes);
+			}
+
+			if (end) {
+				break;
+			}
+		}
+
+		System.out.println(count >= 1001 ? -1 : count);
+	}
 }
