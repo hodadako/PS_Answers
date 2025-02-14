42e3cae76ed2776d93763e92e3d225e9f2230309
Byungryong Ko <kbry12@naver.com>
2025-02-15 03:22:28 +0900

    [Gold IV] Title: DSLR, Time: 7756 ms, Memory: 301952 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,72 +1,95 @@
+import java.io.*;
 import java.util.*;
 
-class Main {
-    static char[] orders = {'D', 'S', 'L', 'R'};
-    static boolean control = true;
-    static boolean[] visited = new boolean[10000];
+public class Main {
+	static int TC, A, B, count = Integer.MAX_VALUE;
+	static StringBuilder answer = new StringBuilder();
 
-    static class Node {
-        int start;
-        StringBuilder result;
+	public static void main(String[] args) {
+		FastReader sc = new FastReader();
+		TC = sc.nextInt();
+		for (int i = 0; i < TC; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			bfs(A, B, "");
+		}
+		System.out.println(answer);
+	}
 
-        public Node(int start, StringBuilder result) {
-            this.start = start;
-            this.result = result;
-        }
-    }
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
 
-    private static int solve(int start, char order) {
-        if (order == 'D') {
-            start *= 2;
-            if (start > 9999) start %= 10000;
-            return start;
-        } else if (order == 'S') {
-            if (start == 0) {
-                start = 9999;
-            } else {
-                start--;
-            }
-            return start;
-        } else if (order == 'L') {
-            return (start % 1000) * 10 + start / 1000;
-        } else {
-            return start / 10 + start % 10 * 1000;
-        }
-    }
-    private static String bfs(int start, int end, StringBuilder list) {
-        LinkedList<Node> q = new LinkedList<>();
-        StringBuilder result = new StringBuilder();
-        q.add(new Node(start, list));
-        visited[start] = true;
-        while (!q.isEmpty() && control) {
-            Node node = q.removeFirst();
-            StringBuilder sb = node.result;
-            for (int i = 0; i < 4; i++) {
-                int cur = solve(node.start, orders[i]);
-                StringBuilder temp = new StringBuilder(sb);
-                temp.append(orders[i]);
-                if (visited[cur]) continue;
-                visited[cur] = true;
-                if (cur == end) {
-                    result = temp;
-                    control = false;
-                } else {
-                    q.add(new Node(cur, temp));
-                }
-            }
-        }
-        return result.toString();
-    }
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        for (int i = 0; i < tc; i++) {
-            int start = sc.nextInt(), end = sc.nextInt();
-            String answer = bfs(start, end, new StringBuilder());
-            control = true;
-            visited = new boolean[10000];
-            System.out.println(answer);
-        }
-    }
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+
+	static class Node {
+		int now;
+		String s;
+
+		public Node(int now, String s) {
+			this.now = now;
+			this.s = s;
+		}
+	}
+
+	static void bfs(int cur, int target, String str) {
+		boolean[] visited = new boolean[10000];
+		String[] values = new String[10000];
+		LinkedList<Node> q = new LinkedList<>();
+		q.add(new Node(cur, str));
+		visited[cur] = true;
+		while (!q.isEmpty()) {
+			Node n = q.poll();
+
+			int d = n.now * 2 > 9999 ? n.now * 2 % 10000 : n.now * 2;
+			int s = n.now - 1 < 0 ? 9999 : n.now - 1;
+			int l = n.now % 1000 * 10 + n.now / 1000;
+			int r = n.now / 10 + n.now % 10 * 1000;
+			
+			if (!visited[d]) {
+				visited[d] = true;
+				values[d] = n.s + "D";
+				q.add(new Node(d, n.s + "D"));
+			}
+
+			if (!visited[s]) {
+				visited[s] = true;
+				values[s] = n.s + "S";
+				q.add(new Node(s, n.s + "S"));
+			}
+
+			if (!visited[l]) {
+				visited[l] = true;
+				values[l] = n.s + "L";
+				q.add(new Node(l, n.s + "L"));
+			}
+
+			if (!visited[r]) {
+				visited[r] = true;
+				values[r] = n.s + "R";
+				q.add(new Node(r, n.s + "R"));
+			}
+		}
+
+		answer.append(values[target]).append("\n");
+	}
 }
