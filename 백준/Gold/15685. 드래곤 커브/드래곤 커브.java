b52c632123d8b2271a8fd28c7983164bfb4bd71c
Byungryong Ko <kbry12@naver.com>
2025-02-19 18:20:01 +0900

    [Gold III] Title: 드래곤 커브, Time: 96 ms, Memory: 13732 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
@@ -1,120 +1,138 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main {
-    static int answer = 0;
-    static int[] dx = {0, -1, 0, 1};
-    static int[] dy = {1, 0, -1, 0};
-    static ArrayList<ArrayList<ArrayList<Integer>>> curves = new ArrayList<>();
-    static int[][] graph = new int[101][101];
-    static boolean[][] visited = new boolean[101][101];
+public class Main {
+	public static class FastReader {
+	    BufferedReader br;
+	    StringTokenizer st;
 
-    static class Node {
-        int x, y;
+	    public FastReader() {
+	        br = new BufferedReader(new InputStreamReader(System.in));
+	    }
 
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+	    String next() {
+	        while (st == null || !st.hasMoreElements()) {
+	            try {
+	                st = new StringTokenizer(br.readLine());
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        return st.nextToken();
+	    }
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
+	    int nextInt() {
+	        return Integer.parseInt(next());
+	    }
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
+	    double nextDouble() {
+	        return Double.parseDouble(next());
+	    }
 
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-    }
+	    long nextLong() {
+	        return Long.parseLong(next());
+	    }
+	}
 
-    private static void init() {
-        for (int i = 0; i <= 10; i++) {
-            curves.add(new ArrayList<>());
-            for (int j = 0; j < 4; j++) {
-                curves.get(i).add(new ArrayList<>());
-            }
-        }
+	static class Node {
+		int x, y, dir;
 
-        for (int i = 0; i < 4; i++) {
-            curves.get(0).get(i).add(i);
-        }
-    }
+		public Node(int x, int y, int dir) {
+			this.x = x;
+			this.y = y;
+			this.dir = dir;
+		}
 
-    private static void add() {
-        for (int i = 1; i <= 10; i++) {
-            int size = curves.get(i - 1).get(0).size();
-            for (int j = 0; j < 4; j++) {
-                if (j == 3) {
-                    curves.get(i).get(j).addAll(curves.get(i - 1).get(j));
-                    for (int k = size - 1; k >= 0; k--) {
-                        curves.get(i).get(j).add(curves.get(i - 1).get(0).get(k));
-                    }
-                } else {
-                    curves.get(i).get(j).addAll(curves.get(i - 1).get(j));
-                    for (int k = size - 1; k >= 0; k--) {
-                        curves.get(i).get(j).add(curves.get(i - 1).get(j + 1).get(k));
-                    }
-                }
-            }
-        }
-    }
+		@Override
+		public String toString() {
+			return "(" + x + ", " + y+ ")";
+		}
+	}
 
-    private static void move(int d, int g, int x, int y) {
-        visited[x][y] = true;
-        int nx = x, ny = y;
-        for (int i : curves.get(g).get(d)) {
-            nx += dx[i];
-            ny += dy[i];
-            visited[nx][ny] = true;
-        }
-    }
+	static int N, x, y;
+	static boolean[][] visited = new boolean[1001][1001];
+	static int[] dx = {0, -1, 0, 1};
+	static int[] dy = {1, 0, -1, 0};
+	static ArrayList<Node> curves = new ArrayList<>();
 
-    private static void count() {
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (visited[i][j] && visited[i + 1][j] && visited[i][j + 1] && visited[i + 1][j + 1]) {
-                    answer++;
-                }
-            }
-        }
-    }
+	static void print() {
+		for (int i = 0; i < 101; i++) {
+			for (int j = 0; j < 101; j++) {
+				System.out.print((visited[i][j] ? 1 : 0) + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("___________________________");
+	}
 
-    private static void print() {
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                System.out.print((visited[i][j] ? 1 : 0) + " ");
-            }
-            System.out.println();
-        }
+	public static void main(String[] args) {
+		FastReader fr = new FastReader();
+		N = fr.nextInt();
+		for (int i = 0; i < N; i++) {
+			y = fr.nextInt() + 10;
+			x = fr.nextInt() + 10;
+			int d = fr.nextInt();
+			int g = fr.nextInt();
+			visited[x][y] = true;
 
-        System.out.println("_______________________________");
-    }
+			for (int j = 0; j < g + 1; j++) {
+				// System.out.println("j = " + j);
+				// System.out.println("x = " + x);
+				// System.out.println("y = " + y);
+				// System.out.println("curves = " + curves);
+				if (j == 0) {
+					x += dx[d];
+					y += dy[d];
+					visited[x][y] = true;
+					curves.add(new Node(dx[d], dy[d], d));
+				} else {
+					addNextGen();
+				}
+				// print();
+			}
+			curves.clear();
+		}
 
-    public static void main(String[] args) {
-        FastReader sc = new FastReader();
-        int n = sc.nextInt();
-        init();
-        add();
+		System.out.println(check());
+	}
 
-        for (int i = 0; i < n; i++) {
-            int x = sc.nextInt(), y = sc.nextInt(), d = sc.nextInt(), g = sc.nextInt();
-            move(d, g, y, x);
-        }
-        count();
-        System.out.println(answer);
-    }
+	private static int check() {
+		int result = 0;
+		for (int i = 0; i < 1000; i++) {
+			for (int j = 0; j < 1000; j++) {
+				if (visited[i][j] && visited[i + 1][j] && visited[i][j + 1] && visited[i + 1][j + 1]) {
+					result++;
+				}
+			}
+		}
+		return result;
+	}
+
+	private static void addNextGen() {
+		ArrayList<Node> temp = new ArrayList<>();
+		Collections.reverse(curves);
+		for (Node n : curves) {
+			int newDir = clockwise(uTurn(n.dir));
+			x += dx[newDir];
+			y += dy[newDir];
+			visited[x][y] = true;
+			temp.add(new Node(dx[newDir], dy[newDir], newDir));
+		}
+		Collections.reverse(curves);
+		ArrayList<Node> next = new ArrayList<>();
+		next.addAll(curves);
+		next.addAll(temp);
+		curves = next;
+	}
+
+	private static int uTurn(int i) {
+		return (i + 2) % 4;
+	}
+
+	private static int clockwise(int i) {
+		if (i - 1 == -1) {
+			return 3;
+		}
+		return i - 1;
+	}
 }
